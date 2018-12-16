2d6c15d782c1070d3dda746c8349cd363a7c505e
R. Tyler Croy <tyler@monkeypox.org>
2018-12-15 21:46:24 -0800

    Add support for expiration for SET

--- acceptance/test_set.py
+++ acceptance/test_set.py
@@ -1,12 +1,20 @@
 #!/usr/bin/env python
 
 import helpers
+import time
 
 class SetTest(helpers.MozzoniTest):
     def test_set(self):
         self.assertTrue(self.r.set('key', 'value'))
         self.assertEqual(self.r.get('key'), 'value')
 
+    def test_set_ex(self):
+        self.assertTrue(
+                self.r.set('keyex', 'value', ex=1)
+                )
+        self.assertEqual(self.r.get('keyex'), 'value')
+        time.sleep(2)
+        self.assertFalse(self.r.get('keyex'), 'value')
 
 if __name__ == '__main__':
     unittest.main()
--- src/mozzoni-commands-keys.adb
+++ src/mozzoni-commands-keys.adb
@@ -3,6 +3,7 @@
 
 with Ada.Text_IO; use Ada.Text_IO;
 
+with Ada.Calendar;
 with Ada.Strings.Unbounded.Hash;
 with Ada.Characters.Latin_1;
 
@@ -13,10 +14,17 @@ with Ada.Text_IO; use Ada.Text_IO;
 
 package body Mozzoni.Commands.Keys is
 
+   No_Expiry : constant Ada.Calendar.Time := Ada.Calendar.Time_Of (1970, 1, 1);
+
+   type Value_Type is record
+      Buffer : Unbounded_String;
+      Expiry : Ada.Calendar.Time := No_Expiry;
+   end record;
+
    package Hashed_Maps is new Ada.Containers.Hashed_Maps (Key_Type        => Unbounded_String,
-                                                         Element_Type    => Unbounded_String,
-                                                         Hash            => Ada.Strings.Unbounded.Hash,
-                                                      Equivalent_Keys => "=");
+                                                          Element_Type    => Value_Type,
+                                                          Hash            => Ada.Strings.Unbounded.Hash,
+                                                          Equivalent_Keys => "=");
    Keys : Hashed_Maps.Map;
 
    -- Set a given value to the specified cache key
@@ -27,14 +35,26 @@ package body Mozzoni.Commands.Keys is
    --    - <value>
    procedure Handle_Set  (Client : in out Client_Type;
                           Command : Command_Array_Access) is
+      use type Ada.Calendar.Time;
+
       Key_Item : Command_Item := Command (2);
-      Value_Item : Command_Item := Command (3);
+      Buffer : Unbounded_String := Command (3).Value;
+      Value_Item : Value_Type;
+      Foo : Float := 1.0;
    begin
+      Value_Item.Buffer := Buffer;
+
+      if Command'Length > 5 then
+         if Command (4).Value = "EX" then
+            Value_Item.Expiry := Ada.Calendar.Clock + 1.0; -- Float'Value (To_String (Command (5).Value));
+         end if;
+      end if;
+
       Put_Line ("Handle_Set");
       if Hashed_Maps.Contains (Keys, Key_Item.Value) then
-         Hashed_Maps.Replace (Keys, Key_Item.Value, Value_Item.Value);
+         Hashed_Maps.Replace (Keys, Key_Item.Value, Value_Item);
       else
-         Hashed_Maps.Insert (Keys, Key_Item.Value, Value_Item.Value);
+         Hashed_Maps.Insert (Keys, Key_Item.Value, Value_Item);
       end if;
 
       Client.Write (Prepare_Response ("+OK"));
@@ -44,14 +64,24 @@ package body Mozzoni.Commands.Keys is
    procedure Handle_Get (Client : in out Client_Type;
                          Command : Command_Array_Access) is
       use Ada.Characters.Latin_1;
+      use type Ada.Calendar.Time;
+
       Key_Item : Command_Item := Command (2);
-      Cached_Value : Unbounded_String := Hashed_Maps.Element (Keys, Key_Item.Value);
+      Cached_Value : Value_Type := Hashed_Maps.Element (Keys, Key_Item.Value);
    begin
       Put_Line ("Handle_Get");
       Client.Write ('$');
-      Client.Write (Length (Cached_Value));
+
+      if Cached_Value.Expiry /= No_Expiry then
+         if Cached_Value.Expiry < Ada.Calendar.Clock then
+            Client.Write (Prepare_Response ("-1"));
+            return;
+         end if;
+      end if;
+
+      Client.Write (Length (Cached_Value.Buffer));
       Client.Write (CR & LF);
-      Client.Write (Prepare_Response (Cached_Value));
+      Client.Write (Prepare_Response (Cached_Value.Buffer));
    end Handle_Get;
 
 end Mozzoni.Commands.Keys;
